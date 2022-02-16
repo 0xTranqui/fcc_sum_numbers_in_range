32a73ca7d04ab5030945ff2f1e90e0c7b39f74d3
tranqui.eth <thinkandchill11@gmail.com>
2022-02-16 15:40:37 -0300

    uploaded better formatted version I got from with-heart

--- main.js
+++ main.js
@@ -5,22 +5,30 @@ function sumAll(arr) {
   let quickSum = 0;
   let secondArr = [];
   let secondSum = 0;
+
   for (let i = 0; i < arr.length; i++) {
-    quickSum += arr[i]
-  }; 
+    quickSum += arr[i];
+  }
+
   if (arr[0] < arr[1]) {
-    for (let i = 0; i < Math.abs(arr[0]-arr[arr.length-1])-1; i++) {
-        arr.splice(i+1, 0, arr[i]+1);
-    }; 
-    for (let i = 1; i < arr.length-1 ; i++) { 
-        secondSum += arr[i];
-    }; return quickSum + secondSum;
+    for (let i = 0; i < Math.abs(arr[0] - arr[arr.length - 1]) - 1; i++) {
+      arr.splice(i + 1, 0, arr[i] + 1);
+    }
+
+    for (let i = 1; i < arr.length - 1; i++) {
+      secondSum += arr[i];
+    }
+
+    return quickSum + secondSum;
   } else {
-    for (let i = 0; i < Math.abs(arr[0]-arr[arr.length-1])-1; i++) {
-          arr.splice(i+1, 0, arr[i]-1);
-    };  
-    for (let i = 1; i < arr.length-1 ; i++) { 
-        secondSum += arr[i];
-    };  return quickSum + secondSum;
-  } 
+    for (let i = 0; i < Math.abs(arr[0] - arr[arr.length - 1]) - 1; i++) {
+      arr.splice(i + 1, 0, arr[i] - 1);
+    }
+
+    for (let i = 1; i < arr.length - 1; i++) {
+      secondSum += arr[i];
+    }
+
+    return quickSum + secondSum;
+  }
 }
