9179dc5839d4ce2054eba684454a2eee2d397d08
tranqui.eth <thinkandchill11@gmail.com>
2022-02-16 15:27:38 -0300

    uploading my answer

---
+++ main.js
@@ -0,0 +1,26 @@
+// js script
+// link to fcc problem https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range
+
+function sumAll(arr) {
+  let quickSum = 0;
+  let secondArr = [];
+  let secondSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    quickSum += arr[i]
+  }; 
+  if (arr[0] < arr[1]) {
+    for (let i = 0; i < Math.abs(arr[0]-arr[arr.length-1])-1; i++) {
+        arr.splice(i+1, 0, arr[i]+1);
+    }; 
+    for (let i = 1; i < arr.length-1 ; i++) { 
+        secondSum += arr[i];
+    }; return quickSum + secondSum;
+  } else {
+    for (let i = 0; i < Math.abs(arr[0]-arr[arr.length-1])-1; i++) {
+          arr.splice(i+1, 0, arr[i]-1);
+    };  
+    for (let i = 1; i < arr.length-1 ; i++) { 
+        secondSum += arr[i];
+    };  return quickSum + secondSum;
+  } 
+}
